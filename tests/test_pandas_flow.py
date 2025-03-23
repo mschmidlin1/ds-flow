e140bea2e6b5bac03fe4a8b94cde99cd1a172368
Mike <mschmidlin1@gmail.com>
2025-03-23 18:37:10 -0400

    added a hello world function

--- tests/test_pandas_flow.py
+++ tests/test_pandas_flow.py
@@ -4,6 +4,7 @@ Tests for the pandas_flow module.
 
 import unittest
 from ds_flow import pandas_flow
+from ds_flow.pandas_flow.hello_world import hello
 
 
 class TestPandasFlow(unittest.TestCase):
@@ -12,6 +13,15 @@ class TestPandasFlow(unittest.TestCase):
     def test_import(self):
         """Test that the module can be imported."""
         self.assertIsNotNone(pandas_flow)
+
+    def test_hello(self):
+        """Test the hello() function returns the expected greeting."""
+        # Arrange & Act
+        result = hello()
+        
+        # Assert
+        self.assertEqual(result, "Hello, World!")
+        self.assertIsInstance(result, str)
 
 
 if __name__ == "__main__":
